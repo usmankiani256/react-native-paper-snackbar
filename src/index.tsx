cc77f6160f1c2c74656d8da1333c13c1a04df402
Usman Kiani <usmankiani256@gmail.com>
2021-08-10 19:06:55 +0500

    update showSnackbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react'
 import PaperSnackbar from './Components/Snackbar'
 
+var emitter = require('tiny-emitter/instance')
+
 const Snackbar = () => {
-  return <PaperSnackbar />
+  const [snackbar, setSnackbar] = React.useState({})
+
+  React.useEffect(() => {
+    emitter.on('Show_Snackbar', function (payload: Object) {
+      setSnackbar(payload)
+    })
+  }, [])
+
+  function onDismiss() {
+    setSnackbar({})
+  }
+  return <PaperSnackbar snackbar={snackbar} onDismiss={onDismiss} />
 }
 
-export default Snackbar
+const showSnackbar = (payload: Object) => {
+  emitter.emit('Show_Snackbar', payload)
+}
+
+export { Snackbar, showSnackbar }
