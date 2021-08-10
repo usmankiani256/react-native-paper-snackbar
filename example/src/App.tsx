09350282fa0c4dcb0a460bfab0fb0e9aa8218cdc
Usman Kiani <usmankiani256@gmail.com>
2021-08-10 19:07:31 +0500

    update app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,19 @@
-import * as React from 'react'
-import Snackbar from 'react-native-paper-snackbar'
+import React, { useEffect } from 'react'
+import { showSnackbar, Snackbar } from 'react-native-paper-snackbar'
+
+const snackbar = {
+  title: 'hello',
+  action: {
+    label: 'Action',
+    onPress: () => console.log('Action was pressed'),
+  },
+  duration: 7000,
+}
 
 const App = () => {
+  useEffect(() => {
+    showSnackbar(snackbar)
+  }, [])
   return <Snackbar />
 }
 
